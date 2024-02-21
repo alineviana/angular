3ede480d6bb829c76be8eebb6221d06e6cf48371
Aline Viana <80078418+alineviana@users.noreply.github.com>
2024-02-21 08:00:14 -0300

    add feats

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -15,8 +16,9 @@ export class ListarPensamentoComponent implements OnInit {
   filtro: string = '';
   favoritos: boolean = false;
   listaFavoritos: Pensamento[] = [];
+  titulo: string = "Meu Mural";
 
-  constructor(private service: PensamentoService) { }
+  constructor(private service: PensamentoService, private router: Router) { }
 
   // ciclo de vida do componente
   ngOnInit(): void {
@@ -44,10 +46,21 @@ export class ListarPensamentoComponent implements OnInit {
     })
   }
 
+  recarregarComponente() {
+    this.favoritos = false;
+    this.paginaAtual = 1;
+
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate([this.router.url]);
+  }
+
   listarFavoritos() {
+    this.titulo = "Meus Favoritos";
     this.favoritos = true;
     this.haMaisPensamentos = true;
     this.paginaAtual = 1;
+
     this.service.listar(this.paginaAtual, this.filtro, this.favoritos).subscribe(listaPensamentosFavoritos => {
       this.listaPensamentos = listaPensamentosFavoritos;
       this.listaFavoritos = this.listaFavoritos;
